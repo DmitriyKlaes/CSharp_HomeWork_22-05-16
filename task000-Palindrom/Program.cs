bdea28402bbe4059181d5ade0e6cd8b597908943
Dmitriy_Klaes <89119584059@mail.ru>
2022-05-19 20:18:15 +0300

    initial commit

---
+++ task000-Palindrom/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+23432 -> да
+12821 -> да
+*/
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(System.Console.ReadLine());
+//int numberSave = number;
+int newNumber = 0;
+for (int i = number; i > 0; i = i / 10)
+{
+    int quot = i % 10;
+    newNumber = (quot * 10) + newNumber;
+    //if (i == 0)
+    //{
+        System.Console.WriteLine(i);
+        System.Console.WriteLine(number);
+        System.Console.WriteLine(newNumber);
+    //}    
+}
