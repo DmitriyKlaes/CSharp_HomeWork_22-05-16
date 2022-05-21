63155b58b5b87b0a2d6f944fca2884d90ee2c3c8
Dmitriy_Klaes <89119584059@mail.ru>
2022-05-21 14:27:04 +0300

    task002-CubeTable

---
+++ task002-CubeTable/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 4, 9.
+5 -> 1, 8, 27, 64, 125
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+void CubeTable(int number)
+{
+    Console.Write($"Кубы чисел от 1 до {number}: ");
+    if (number > 0)
+    {
+        for (int i = 1; i <= number; i++)
+        {
+            int result = i * i * i;
+            Console.Write(result);
+            if (i <= number - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+    }
+    else
+    {
+        for (int i = 1; i >= number; i--)
+        {
+            int result = i * i * i;
+            Console.Write(result);
+            if (i >= number + 1)
+            {
+                Console.Write(", ");
+            }
+        }
+    }
+}
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+CubeTable(number);
